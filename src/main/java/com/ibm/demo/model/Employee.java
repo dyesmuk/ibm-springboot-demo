388d0c764e352b1aa36d716043472c2d1c41e764
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-07 11:36:50 +0530

    model

--- src/main/java/com/ibm/demo/model/Employee.java
+++ src/main/java/com/ibm/demo/model/Employee.java
@@ -1,5 +1,11 @@
 package com.ibm.demo.model;
 
 public class Employee {
+	
+	private int id;
+	private String name;
+	private double salary;
+	
+	// constructors, getters, setters, toString etc 
 
 }
