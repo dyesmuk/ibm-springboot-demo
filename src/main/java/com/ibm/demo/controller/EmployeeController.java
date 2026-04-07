7b6adb61ab3dd05b5845e4e913c031739b249260
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-07 12:06:02 +0530

    employee methods

--- src/main/java/com/ibm/demo/controller/EmployeeController.java
+++ src/main/java/com/ibm/demo/controller/EmployeeController.java
@@ -1,5 +1,41 @@
 package com.ibm.demo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.demo.model.Employee;
+import com.ibm.demo.service.EmployeeService;
+
+@RestController
 public class EmployeeController {
+
+	private EmployeeService empService;
+
+	@GetMapping("employees")
+	public Employee getEmployeeById() {
+		int employeeId = 1;
+		return empService.getEmployeeById(employeeId);
+	}
+
+//	@GetMapping("employees")
+//	public List<Employee> getAllEmployees() {
+//		return empService.getAllEmployees();
+//	}
+//
+//	public Employee addEmployee(Employee employee) {
+//		return empService.addEmployee(employee);
+//	}
+//
+//	public Employee updateEmployee(Employee employee) {
+//		return empService.updateEmployee(employee);
+//	}
+//
+//	public Employee deleteEmployee(int id) {
+//		int employeeId = 2;
+//		return empService.deleteEmployee(employeeId);
+//	}
 
 }
